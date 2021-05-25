59d241af661f1ea4707bbbc33c25e75880eafab5
Evgeniy Chernyshenko <magdegaben@gmail.com>
2021-05-25 17:15:45 +0300

    adding mongodb logic

--- bin/server.js
+++ bin/server.js
@@ -1,7 +1,11 @@
 const app = require('../src/app')
+const db = require('../src/db')
 
 const PORT = process.env.PORT || 3600
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+db.then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`)
+  })
+}).catch((e) => { console.log(`Server not running ${e.message}`) }
+)
